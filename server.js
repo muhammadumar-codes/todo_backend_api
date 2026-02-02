f34f7a1c06bd606daaf8b22dbfef28aa1bb70650
muhammadumar-codes <muhammadumar.codes@gmail.com>
2026-02-01 16:31:23 -0800

    update

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+require("dotenv").config();
 const app = require("./app");
+const connectDB = require("./config/db");
 
-const PORT = 5000;
+// Connect to MongoDB
+connectDB();
 
+// Start the server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`✅ Server started on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
