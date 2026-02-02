2c7972c44134f0a854e22a51d9077bd9e8d299a4
muhammadumar-codes <muhammadumar.codes@gmail.com>
2026-02-02 11:12:05 -0800

    update

--- config/db.js
+++ config/db.js
@@ -1,14 +1,21 @@
+
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    
-    console.log(" MongoDB Connected");
-  } catch (error) {
-    console.error(" MongoDB Connection Failed:", error);
-    process.exit(1); // Exit the process if DB fails to connect
-  }
-};
+
+
+  const DBURL=process.env.MONGO_URI
+  
+  
+  
+  async function connectDB() {
+    try {
+      await mongoose.connect(DBURL);
+     
+      console.log(" MongoDB Connected Successfully");
+    } catch (error) {
+      console.error(" MongoDB Connection Failed:", error.message);
+      process.exit(1);
+    }
+  };
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./config/db");
 
-// Connect to MongoDB
+// Connect DB
 connectDB();
 
-// Start the server
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
