22a033109185721ecc2b2ac868e9f882e8ad7417
muhammadumar-codes <muhammadumar.codes@gmail.com>
2026-02-06 14:31:55 +0500

    update

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
 const express = require('express')
-// const cors = require('cors')
 
 const authRoutes = require('./routes/auth.routes')
 const todoRoutes = require('./routes/todo.routes')
 
 const app = express()
+
+// body parser
 app.use(express.json())
 
-// app.use(
-//   cors({
-//     origin: ['http://localhost:3000', 'https://todo-fullstack-nine-tau.vercel.app'],
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     credentials: true,
-//   }),
-// )
-
-//  Public routes
+// routes
 app.use('/api/auth', authRoutes)
-
-// Protected routes
 app.use('/api/todos', todoRoutes)
 
+// health check
 app.get('/', (req, res) => {
   res.send('Todo Backend API is running 🚀')
 })
--- server.js
+++ server.js
@@ -1,35 +1,29 @@
-const PORT = process.env.PORT || 5000
-const app = require('./app')
 const dotenv = require('dotenv')
 const cors = require('cors')
+const dns = require('dns')
+
 const connectDB = require('./config/db')
+const app = require('./app')
 
 dotenv.config()
 
-const dns = require('dns')
-
 dns.setServers(['8.8.8.8', '8.8.4.4'])
+
+const PORT = process.env.PORT || 5000
 
 // CORS
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://todo-fullstack-nine-tau.vercel.app'],
+    origin: ['http://localhost:5000', 'https://todo-fullstack-nine-tau.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   }),
 )
 
-// connect databasex`
+// connect database
 connectDB()
 
-// routes
-app.use('/api/auth', require('./routes/auth.routes'))
-app.use('/api/todos', require('./routes/todo.routes'))
-
-app.get('/', (req, res) => {
-  res.send('Todo Backend API is running 🚀')
-})
-
+// start server
 app.listen(PORT, () => {
   console.log(` Server running on port ${PORT}`)
 })
