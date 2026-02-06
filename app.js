fa8ec1490a1a1ac04483b038539d98be7736288d
muhammadumar-codes <muhammadumar.codes@gmail.com>
2026-02-06 10:32:21 +0500

    update

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express')
-// const cors = require("cors");
+const cors = require('cors')
 
 const authRoutes = require('./routes/auth.routes')
+const todoRoutes = require('./routes/todo.routes')
 
 const app = express()
 
-// app.use(cors());
 app.use(express.json())
 
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'https://todo-fullstack-nine-tau.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  }),
+)
+
+//  Public routes
 app.use('/api/auth', authRoutes)
+
+// Protected routes
+app.use('/api/todos', todoRoutes)
+
+app.get('/', (req, res) => {
+  res.send('Todo Backend API is running 🚀')
+})
 
 module.exports = app
