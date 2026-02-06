518aa5d0a32673834c86c957efeeba24ece038f5
muhammadumar-codes <muhammadumar.codes@gmail.com>
2026-02-06 09:59:35 +0500

    update

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ connectDB()
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/todos', require('./routes/todo.routes'))
 
+app.get('/', (req, res) => {
+  res.send('Todo Backend API is running 🚀')
+})
+
 app.listen(PORT, () => {
   console.log(` Server running on port ${PORT}`)
 })
