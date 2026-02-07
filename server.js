a1ba4e1f5fb56d6ba1b97954e88e8f8a840bd3ec
muhammadumar-codes <muhammadumar.codes@gmail.com>
2026-02-07 16:01:03 +0500

    update cors

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT
 
 app.use(
   cors({
-    origin: '*',
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://your-frontend-domain.com'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
   }),
 )
 
