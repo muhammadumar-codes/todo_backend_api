ee0b17e5235bc0fa88cdf74042c574db273f5859
muhammadumar-codes <muhammadumar.codes@gmail.com>
2026-02-16 17:24:42 +0500

    update the todo routes

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -1,17 +1,30 @@
+// =================== todo.routes.js ===================
+import express from 'express'
+const router = express.Router()
+
+import authMiddleware from '../middlewares/auth.middleware.js'
 import { validate } from '../middlewares/validate.middleware.js'
+
+import {
+  createTodo,
+  getTodos,
+  updateTodo,
+  deleteTodo,
+} from '../controllers/todo.controller.js'
+
 import {
   createTodoSchema,
   updateTodoSchema,
   todoIdParamSchema,
 } from '../validations/todo.validation.js'
 
-// =====*** Create Todo ***=====
+// =====*** Create Todo (Validate Body) ***=====
 router.post('/', authMiddleware, validate(createTodoSchema), createTodo)
 
 // =====*** Get Todos (No body validation needed) ***=====
 router.get('/', authMiddleware, getTodos)
 
-// =====*** Update Todo ***=====
+// =====*** Update Todo (Validate Params + Body) ***=====
 router.put(
   '/:id',
   authMiddleware,
@@ -20,10 +33,12 @@ router.put(
   updateTodo
 )
 
-// =====*** Delete Todo ***=====
+// =====*** Delete Todo (Validate Params) ***=====
 router.delete(
   '/:id',
   authMiddleware,
   validate(todoIdParamSchema, 'params'),
   deleteTodo
 )
+
+export default router
